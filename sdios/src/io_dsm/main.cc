7290753e472fd040288fd74a73126cffffc35017
Jan Tepelmann <JanTepelmann@web.de>
2008-06-26 14:34:48 +0000

    server gegben nur noch machen nur noch printf("bla is alive")
    git-svn-id: https://vcs.services.xcore.net/svn/sdi/trunk@16 4e2da4a0-07c1-457e-aff5-4448b6cc6ca9

--- sdios/src/io_dsm/main.cc
+++ sdios/src/io_dsm/main.cc
@@ -1,7 +1,7 @@
 //
-// File:  src/test/main.cc
+// File:  src/io_dsm/main.cc
 //
-// Description: Testclient
+// Description: IO-DataspaceManager
 //
 
 #include <l4/thread.h>
@@ -17,25 +17,8 @@
 L4_ThreadId_t locatorid; 
 
 int main () {
-    printf ("Testclient is alive\n");
+    printf ("IO-DataspaceManager is alive\n");
     
-    /* Guess locatorid */
-    locatorid = L4_GlobalId (L4_ThreadIdUserBase (L4_KernelInterface ()) + 3, 1);
-
-    CORBA_Environment env (idl4_default_environment);    
-    L4_ThreadId_t loggerid = L4_nilthread;
-
-    printf ("Resolve logger ...\n");
-    while (L4_IsNilThread (loggerid)) {
-	IF_LOCATOR_Locate ((CORBA_Object)locatorid, IF_LOGGING_ID, &loggerid, &env);
-    }
-
-    /* Printout message through logger */
-    IF_LOGGING_LogMessage ((CORBA_Object)loggerid, "Hello, this is your testclient", &env);
-
-    /* Now try our sdi lib */
-    LogMessage ("Hello this is your lib");
-
     /* Spin forever */
     while (42);
     
